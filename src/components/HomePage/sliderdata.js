94a17ed828be4aeec4b96cc5a9379e166028ea1f
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2020-11-03 11:23:48 +0100

    add data for sliders in sliderdata folder

---
+++ src/components/HomePage/sliderdata.js
@@ -0,0 +1,34 @@
+export const sliderdata = [
+  {
+    id: 1,
+    img:
+      "https://images.unsplash.com/photo-1567579518027-7a113279f8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80",
+    title: "Slider one",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, repellat. Nam obcaecati ipsam sit harum aspernatur amet quidem, ratione id!",
+  },
+  {
+    id: 2,
+    img:
+      "https://images.unsplash.com/photo-1457382713369-161d1d986f54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
+    title: "Slider two",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, repellat. Nam obcaecati ipsam sit harum aspernatur amet quidem, ratione id!",
+  },
+  {
+    id: 3,
+    img:
+      "https://images.unsplash.com/photo-1549044466-404f19605ddc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    title: "Slider three",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, repellat. Nam obcaecati ipsam sit harum aspernatur amet quidem, ratione id!",
+  },
+  {
+    id: 4,
+    img:
+      "https://images.unsplash.com/photo-1542634093-e0198d4d1e46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    title: "Slider four",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, repellat. Nam obcaecati ipsam sit harum aspernatur amet quidem, ratione id!",
+  },
+];
