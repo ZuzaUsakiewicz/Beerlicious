631f20d4cd021886131720fed338773d6c0265eb
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2020-11-03 15:29:30 +0100

    add functionality and style to Slider and slider buttons

--- src/components/HomePage/HeroSection.styled.js
+++ src/components/HomePage/HeroSection.styled.js
@@ -14,6 +14,7 @@ export const SliderContainer = styled.div`
 `;
 
 export const SliderContent = styled.div`
+  padding-top: 30px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -25,4 +26,40 @@ export const SliderContent = styled.div`
   background-position: center;
   background-size: cover;
   box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.4);
+  transition: all 0.5s ease-in-out;
+`;
+
+export const ButtonPrev = styled.button`
+  border: none;
+  border-radius: 10px;
+  background: white;
+  padding: 20px 40px;
+  position: absolute;
+  top: 50%;
+  left: 5%;
+  cursor: pointer;
+`;
+
+export const ButtonNext = styled.button`
+  border: none;
+  border-radius: 10px;
+  background: white;
+  padding: 20px 40px;
+  position: absolute;
+  top: 50%;
+  right: 5%;
+  cursor: pointer;
+`;
+export const TextContainer = styled.div`
+  width: 60%;
+  height: 90%;
+`;
+
+export const SliderTitle = styled.h1`
+  font-size: ${({ theme }) => theme.fontSize.huge};
+`;
+
+export const SliderParagraph = styled.p`
+  font-weight: ${({ theme }) => theme.fontWeight.thin};
+  font-size: ${({ theme }) => theme.fontSize.regular};
 `;
--- src/components/HomePage/Slider.js
+++ src/components/HomePage/Slider.js
@@ -1,16 +1,49 @@
 import React, { useState } from "react";
-import { SliderContent } from "./HeroSection.styled";
+import {
+  SliderContent,
+  ButtonPrev,
+  ButtonNext,
+  SliderTitle,
+  SliderParagraph,
+  TextContainer,
+} from "./HeroSection.styled";
 import sliderdata from "./sliderdata";
 
 const Slider = () => {
   const [index, setIndex] = useState(0);
   const { id, img, title, text } = sliderdata[index];
+
+  const checkNumber = (number) => {
+    if (number > sliderdata.length - 1) {
+      return 0;
+    } else if (number < 0) {
+      return sliderdata.length - 1;
+    }
+    return number;
+  };
+
+  const clickPrev = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const clickNext = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
   return (
     <SliderContent img={img} key={id}>
-      <h1>{title}</h1>
-      <p>{text}</p>
-      <button> prev </button>
-      <button>next </button>
+      <TextContainer>
+        <SliderTitle>{title}</SliderTitle>
+        <SliderParagraph>{text}</SliderParagraph>
+      </TextContainer>
+      <ButtonPrev onClick={clickPrev}> prev </ButtonPrev>
+      <ButtonNext onClick={clickNext}> next </ButtonNext>
     </SliderContent>
   );
 };
