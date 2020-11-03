bd002dacb88746d67e2cd18207c1797036153dd6
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2020-11-03 19:15:17 +0100

    add files for InfoSection in HomePage folder

---
+++ src/components/HomePage/InfoSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { InfoSectionContainer } from "./InfoSection.styled";
+
+const InfoSection = () => {
+  return (
+    <InfoSectionContainer>
+      <h1>helloo</h1>
+    </InfoSectionContainer>
+  );
+};
+
+export default InfoSection;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../components/HomePage/HeroSection";
+import InfoSection from "../components/HomePage/InfoSection";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <InfoSection />
     </>
   );
 };
