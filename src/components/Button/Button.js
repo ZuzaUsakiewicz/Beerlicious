9f04acbb77403a77a67461fb84c7ac52e737fc89
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2020-11-07 12:59:28 +0100

    change button styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,28 +1,20 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-// export const Button = styled.button`
-//   border-radius: 10px;
-//   border: none;
-//   background: ${({ primary }) => (primary ? "red" : "blue")};
-//   color: ${({ mainfont }) => (mainfont ? "blue" : "red")};
-//   font-size: 40px;
-//   cursor: pointer;
-// `;
-
 export const Button = styled(Link)`
   text-decoration: none;
+  text-transform: uppercase;
   background: ${({ primary, theme }) =>
-    primary ? theme.colors.primary : theme.colors.dark};
+    primary ? theme.colors.orange : theme.colors.dark};
   padding: 10px 20px;
   color: ${({ primary, theme }) =>
-    primary ? theme.colors.dark : theme.colors.primary};
+    primary ? theme.colors.dark : theme.colors.orange};
   transition: color 0.4s ease-in-out, background 0.4s ease-in-out;
   &:hover {
     background: ${({ primary, theme }) =>
-      primary ? theme.colors.dark : theme.colors.primary};
+      primary ? theme.colors.dark : theme.colors.orange};
     padding: 10px 20px;
     color: ${({ primary, theme }) =>
-      primary ? theme.colors.primary : theme.colors.dark};
+      primary ? theme.colors.orange : theme.colors.dark};
   }
 `;
