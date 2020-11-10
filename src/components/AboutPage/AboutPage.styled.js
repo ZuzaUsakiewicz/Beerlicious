58ccdefa1ef4bb7bb5e900299b074a94cead3c12
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2020-11-10 20:23:19 +0100

    add parallax to about page

--- src/components/AboutPage/AboutPage.styled.js
+++ src/components/AboutPage/AboutPage.styled.js
@@ -1,5 +1,28 @@
 import styled from "styled-components";
 
 export const AboutSectionContainer = styled.section`
-  font-size: 20px;
+  width: 100%;
+`;
+
+export const TitleSection = styled.div`
+  color: white;
+  padding: 2rem 1rem;
+  height: 100vh;
+  background-image: url("https://images.unsplash.com/photo-1436076863939-06870fe779c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80");
+  min-height: 500px;
+  box-shadow: inset 0 0 0 1000px rgba(9, 67, 109, 0.63);
+  background-attachment: fixed;
+  background-position: 0 0;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+export const HistorySection = styled(TitleSection)`
+  padding: 0 1rem;
+  background-image: url("https://images.unsplash.com/photo-1571613316887-6f8d5cbf7ef7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80");
+`;
+
+export const FutureSection = styled(TitleSection)`
+  padding: 0 1rem;
+  background-image: url("https://images.unsplash.com/photo-1584225064536-d0fbc0a10f18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80");
 `;
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,14 +1,41 @@
 import React from "react";
-import { AboutSectionContainer } from "../components/AboutPage/AboutPage.styled";
+import {
+  AboutSectionContainer,
+  TitleSection,
+  HistorySection,
+  FutureSection,
+} from "../components/AboutPage/AboutPage.styled";
+import { Title } from "../layout/Layout";
 
 const AboutPage = () => {
   return (
     <AboutSectionContainer>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facilis
-        cumque earum, natus molestiae ad ipsa sit neque ipsum recusandae.
-      </p>
+      <TitleSection>
+        <Title>About</Title>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facilis
+          cumque earum, natus molestiae ad ipsa sit neque ipsum recusandae.
+        </p>
+      </TitleSection>
+      <HistorySection>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium,
+          adipisci exercitationem. Perferendis magni voluptatem repudiandae
+          temporibus. Error ipsa placeat nesciunt tempora doloremque ullam animi
+          ea, saepe, doloribus reiciendis rerum! Cum, nemo soluta! Adipisci,
+          tempore corrupti. Dolorem dolores rerum tempora doloribus.
+        </p>
+      </HistorySection>
+      <FutureSection>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis,
+          repellat! Facilis, repudiandae accusantium aliquam ratione, quod ipsam
+          iusto voluptatem reiciendis aspernatur voluptates omnis rerum
+          voluptatum tenetur dolorem consequuntur nisi enim eius id fuga,
+          laborum pariatur est maxime neque velit. Sequi aut aliquid
+          reprehenderit et magnam illum, autem accusantium! Vitae, minima.
+        </p>
+      </FutureSection>
     </AboutSectionContainer>
   );
 };
