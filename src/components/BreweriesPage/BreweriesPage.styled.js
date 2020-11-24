2dbb2cbfc9015c29be754ef255e30e942d92a1b0
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2020-11-24 14:47:19 +0100

    fetch second api

---
+++ src/components/BreweriesPage/BreweriesPage.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const BreweriesPageContainer = styled.section`
+  width: 100%;
+  min-height: 110vh;
+  padding: 10rem 1rem;
+`;
--- src/pages/BreweriesPage.js
+++ src/pages/BreweriesPage.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BreweriesPageContainer } from "../components/BreweriesPage/BreweriesPage.styled";
+import { Title } from "../layout/Layout";
 
 const BreweriesPage = () => {
-  return <h1>Breweries</h1>;
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [breweries, setBreweries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.openbrewerydb.org/breweries?by_type=brewpub")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setBreweries(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+  if (error) {
+    return (
+      <BreweriesPageContainer>
+        <Title>Breweries</Title>
+        <h1>Couldn't load the page...</h1>
+      </BreweriesPageContainer>
+    );
+  } else if (!isLoaded) {
+    return (
+      <BreweriesPageContainer>
+        <Title>Breweries</Title>
+        <h1>Loading...</h1>
+      </BreweriesPageContainer>
+    );
+  } else {
+    return (
+      <BreweriesPageContainer>
+        <Title>Breweries</Title>
+        {breweries.map((brew) => {
+          return <p>{brew.name}</p>;
+        })}
+      </BreweriesPageContainer>
+    );
+  }
 };
 
 export default BreweriesPage;
