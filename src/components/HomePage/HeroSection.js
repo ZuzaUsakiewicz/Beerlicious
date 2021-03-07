ef0fa6ce05645e528d8439c2f568efef52eeb5af
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-03-07 15:53:17 +0100

    changed HeroSection slider to React Elastic Carousel

--- src/components/HomePage/HeroSection.js
+++ src/components/HomePage/HeroSection.js
@@ -1,12 +1,33 @@
 import React from "react";
-import Slider from "./Slider";
 import { MainSection, SliderContainer } from "./HeroSection.styled";
-
+import { Title } from "../../layout/Layout";
+import {
+  SliderContent,
+  SliderParagraph,
+  TextContainer,
+} from "./HeroSection.styled";
+import { Button } from "../Button/Button";
+import Carousel from "react-elastic-carousel";
+import sliderdata from "./sliderdata";
 const HeroSection = () => {
   return (
     <MainSection>
       <SliderContainer>
-        <Slider />
+        <Carousel itemsToShow={1}>
+          {sliderdata.map((item) => {
+            return (
+              <SliderContent img={item.img} key={item.id}>
+                <TextContainer>
+                  <Title>{item.title}</Title>
+                  <SliderParagraph>{item.text}</SliderParagraph>
+                  <Button primary="true" to={item.url}>
+                    read more
+                  </Button>
+                </TextContainer>
+              </SliderContent>
+            );
+          })}
+        </Carousel>
       </SliderContainer>
     </MainSection>
   );
