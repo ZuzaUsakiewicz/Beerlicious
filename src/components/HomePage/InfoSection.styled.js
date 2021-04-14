6e8e3f5085407c3a0207d0203a2fb319edd96f7f
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-04-14 16:21:04 +0200

    fixed image source, width and background color

--- src/components/HomePage/InfoSection.styled.js
+++ src/components/HomePage/InfoSection.styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import InfoImage from "../../images/jon-parry-epaXKn3gfQo-unsplash.jpg";
 
 export const InfoSectionContainer = styled.section`
   width: 100%;
@@ -8,6 +9,7 @@ export const InfoSectionContainer = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: ${({ theme }) => theme.colors.dark};
   @media screen and (max-width: 960px) {
     min-height: 120vh;
     flex-direction: column;
@@ -27,9 +29,7 @@ export const InfoText = styled.div`
 `;
 
 export const Informations = styled.p`
-  line-height: 25px;
   padding: 2rem 0;
-  font-size: 1rem;
   text-align: justify;
   font-weight: ${({ theme }) => theme.fontWeight.thin};
   @media screen and (max-width: 960px) {
@@ -39,11 +39,13 @@ export const Informations = styled.p`
 export const ImageContainer = styled.div`
   width: 50%;
   height: 80%;
-  background: url("https://images.unsplash.com/photo-1551797802-f2dd1ec0033e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80");
+  background-image: url(${InfoImage});
   background-repeat: no-repeat;
-  background-position: center;
+  background-position: 20% 50%;
   background-size: cover;
-  box-shadow: -2px 2px 20px 10px rgba(23, 55, 83, 0.6);
+  @media screen and (max-width: 960px) {
+    width: 100%;
+  }
 `;
 
 export const LinkAbout = styled(Link)`
