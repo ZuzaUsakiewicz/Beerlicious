5ed2b11a636b85b3dfc84a9590d545bb05b9d63e
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-04-22 12:15:52 +0200

    add background image to list of links

--- src/components/BreweriesPage/BreweriesPage.styled.js
+++ src/components/BreweriesPage/BreweriesPage.styled.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import BgImage from "../../images/monica-di-loxley-ddsRNqy4m5Y-unsplash.jpg";
+
 export const BreweriesPageContainer = styled(motion.section)`
   width: 100%;
   min-height: 110vh;
@@ -12,8 +14,14 @@ export const BrewContentContainer = styled.div`
   padding: 2rem;
   display: flex;
   flex-wrap: wrap;
+  margin-top: 1.5rem;
   justify-content: space-evenly;
   align-items: center;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  box-shadow: inset -2px 2px 20px 1000px rgba(29, 112, 162, 0.788);
+  background-image: url(${BgImage});
 `;
 
 export const BrewLink = styled.a`
